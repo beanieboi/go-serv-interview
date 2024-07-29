3c3f7f0528a306fbcf61b953d3fb35d235175e19
Ben Fritsch <beanie@benle.de>
2024-07-29 18:04:24 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/beanieboi/go-serv-interview
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"os"
+)
+
+type Response struct {
+	ID string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		w.Header().Set("Content-Type", "application/json")
+		if rand.Intn(2) == 0 {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+		json.NewEncoder(w).Encode(Response{ID: id})
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
+	http.ListenAndServe(":"+port, mux)
+}
